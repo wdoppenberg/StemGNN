b0013287d47fa233be9ba4d292de4abdf71fc8ac
wdoppenberg <wouter.doppenberg@ah.nl>
2022-05-19 16:18:48 +0200

    Fixed FFT calls (PyTorch breaking change) https://github.com/microsoft/StemGNN/issues/14

--- models/base_model.py
+++ models/base_model.py
@@ -46,16 +46,20 @@ class StockBlockLayer(nn.Module):
     def spe_seq_cell(self, input):
         batch_size, k, input_channel, node_cnt, time_step = input.size()
         input = input.view(batch_size, -1, node_cnt, time_step)
-        ffted = torch.rfft(input, 1, onesided=False)
+        #discrete fourier transform
+        ffted = torch.view_as_real(torch.fft.fft(input, dim=1))
+        #1DConv
         real = ffted[..., 0].permute(0, 2, 1, 3).contiguous().reshape(batch_size, node_cnt, -1)
         img = ffted[..., 1].permute(0, 2, 1, 3).contiguous().reshape(batch_size, node_cnt, -1)
+        #GLU
         for i in range(3):
             real = self.GLUs[i * 2](real)
             img = self.GLUs[2 * i + 1](img)
         real = real.reshape(batch_size, node_cnt, 4, -1).permute(0, 2, 1, 3).contiguous()
         img = img.reshape(batch_size, node_cnt, 4, -1).permute(0, 2, 1, 3).contiguous()
         time_step_as_inner = torch.cat([real.unsqueeze(-1), img.unsqueeze(-1)], dim=-1)
-        iffted = torch.irfft(time_step_as_inner, 1, onesided=False)
+        #inverse DFT
+        iffted = torch.fft.irfft(torch.view_as_complex(time_step_as_inner), n=time_step_as_inner.shape[1], dim=1)
         return iffted
 
     def forward(self, x, mul_L):
